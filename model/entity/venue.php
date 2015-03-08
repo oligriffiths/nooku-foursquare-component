2412097e693bd1594ff0e172fdddc06306c51f2a
Oli Griffiths <oli.griffiths@me.com>
2015-03-07 20:40:31 -0500

    Added getter methods for venue location

--- model/entity/venue.php
+++ model/entity/venue.php
@@ -30,4 +30,54 @@ class ModelEntityVenue extends Library\ModelEntityAbstract
     {
         return (float) ($this->location && isset($this->location['lng']) ? $this->location['lng'] : 0);
     }
+
+    /**
+     * Gets the address
+     *
+     * @return null|string
+     */
+    public function getAddress()
+    {
+        return $this->location && isset($this->location['address']) ? $this->location['address'] : null;
+    }
+
+    /**
+     * Gets the city
+     *
+     * @return null|string
+     */
+    public function getCity()
+    {
+        return $this->location && isset($this->location['city']) ? $this->location['city'] : null;
+    }
+
+    /**
+     * Gets the state
+     *
+     * @return null|string
+     */
+    public function getState()
+    {
+        return $this->location && isset($this->location['state']) ? $this->location['state'] : null;
+    }
+
+    /**
+     * Gets the postal code
+     *
+     * @return null|string
+     */
+    public function getPostalcode()
+    {
+        return $this->location && isset($this->location['postal_code']) ? $this->location['postal_code'] : null;
+    }
+
+    /**
+     * Gets the country
+     *
+     * @return null|string
+     */
+    public function getCountry()
+    {
+        return $this->location && isset($this->location['cc']) ? $this->location['cc'] : null;
+    }
 }
